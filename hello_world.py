2559db251983f2acb5cfd8c0c37d6413c55d208b
darshan-M-tech <darshandarshan84356@gmail.com>
2026-01-15 11:35:03 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+from datetime import date #importing datetime library to fetch today's date
+name=input("Enter Your Name: ") #It store the name entered by the user
+internship_role=input("Enter your internship role:") #It store the internship role  entered by the user
+today_date= date.today() #It holds the today's date
+print("Your Name: ",name)#print's the name
+print("Your Internship Role: ",internship_role)#print's the internship role
+print("Today's Date: ",today_date)#prints the today's date
